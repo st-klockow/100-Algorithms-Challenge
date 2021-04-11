116138e533bc7d5942727e94b4406e55e88cd829
Steve Klockow <steve.klockow@gmail.com>
2021-04-11 15:26:31 -0700

    addtwoDigits completed

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
 export function addTwoDigits(n: any): number {
-
+    const nums= n.toString().split('');
+    
+    return nums.reduce((a: String, b:string) => {
+        return parseInt(a) + parseInt(b);
+    });
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
