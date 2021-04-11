9d6c5dba19be71632bda1981d5978e8be2fcba84
Steve Klockow <steve.klockow@gmail.com>
2021-04-11 15:57:28 -0700

    allLongestStrings completed

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,17 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+    let largestStringLength = 0;
+    let largestStrings = [];
+    
+    inputArray.forEach((word: string) => {
+        largestStringLength = largestStringLength < word.length ? word.length : largestStringLength;
+    });
 
+    inputArray.forEach((word: string) => {
+        if(word.length === largestStringLength){
+            largestStrings.push(word);
+        }
+    });
+    return largestStrings;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
