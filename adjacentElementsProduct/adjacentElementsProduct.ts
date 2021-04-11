6579fac577537c57648ca4dcc44a631545d253be
Steve Klockow <steve.klockow@gmail.com>
2021-04-11 15:39:03 -0700

    adjacentElementsProduct completed

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,13 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+    let biggestProduct = inputArray[0] * inputArray[1];
+    
+    for(let i = 0; i < inputArray.length; i++){
+        if((inputArray[i] * inputArray[i+1]) > biggestProduct){
+            biggestProduct = inputArray[i] * inputArray[i+1];
+        }
+    }
 
+    return biggestProduct;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
