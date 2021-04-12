d62f5548eaeeea1f2510c59e6e6928db545b4b2e
Steve Klockow <steve.klockow@gmail.com>
2021-04-12 05:02:47 -0700

    almostIncreasingSequence completed

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,14 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+    let countErrors: number = 0;
 
+    for(let i = 0; i < sequence.length; i++){
+        if(sequence[i] > (sequence[i+1])){
+            countErrors++;
+        }
+    }
+
+    return countErrors <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
