b83148515d990dc849d128e415269cbcf86f30d0
Steve Klockow <steve.klockow@gmail.com>
2021-04-12 05:43:19 -0700

    alphabeticShift completed

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,19 @@
 export function alphabeticShift(inputString: string): string {
+    let newArray: string = [];
+    const alphabet: string = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+    let inputStringArray = inputString.split("");
+    for(let i = 0; i < inputString.length; i++){
+        let letterNum = alphabet.indexOf(inputStringArray[i]);
+        
+        if((letterNum + 1) == alphabet.length){
+            newArray.push(alphabet[0]);   
+        } else {
+            newArray.push(alphabet[letterNum + 1]);
+        }
+    }
+
+    return newArray.join("");
 }
 
 console.log(alphabeticShift('crazy'));
