4caf5ed8ec3b77381253b14b5834aaca0a1d5df4
Steve Klockow <steve.klockow@gmail.com>
2021-04-14 10:43:56 +0200

    areEquallyStrong completed

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,15 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    const yourStrongest: number = yourLeft > yourRight ? yourLeft : yourRight;
+    const yourWeakest: number = yourLeft < yourRight ? yourLeft : yourRight;
+    const friendsStrongest: number = friendsLeft > friendsRight ? friendsLeft : friendsRight;
+    const friendsWeakest: number = friendsLeft < friendsRight ? friendsLeft : friendsRight;
 
+    if(yourStrongest !== friendsStrongest || yourWeakest !== friendsWeakest){
+        return false;
+    }
+    return true;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
