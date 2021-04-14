5fbd5ce712889bafd015c62cf477471e31919735
Steve Klockow <steve.klockow@gmail.com>
2021-04-14 06:57:20 +0200

    alternatingSum completed

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,14 @@
 export function alternatingSums(a: number[]): number[] {
+    let teamWeight: number[] = [];
+    let weightTeam1: number = 0;
+    let weightTeam2: number = 0;
+    for(let i = 0; i < a.length; i++){
+        if(i % 2 === 0) weightTeam1 += a[i];
+        else weightTeam2 += a[i];
+    }
 
+    teamWeight.push(weightTeam1, weightTeam2)
+    return teamWeight;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
