46abf3c7409b556b0dbde0dcb9575a370c6a1ef8
Steve Klockow <steve.klockow@gmail.com>
2021-04-15 19:35:01 +0200

    areSimilar completed

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,14 @@
 export function areSimilar(a: number[], b: number[]): boolean {
-
+    a.sort();
+    b.sort();
+    for(let i = 0; i < a.length; i++){
+        if(a[i] !== b[i]){
+            return false
+        }
+    }
+    return true;
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
