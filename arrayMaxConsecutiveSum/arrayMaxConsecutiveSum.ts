94e338ce1c6eb9af76e429845a421992452ea283
Steve Klockow <steveklockow@FATCHIPs-MBP.fritz.box>
2021-04-20 00:26:46 +0200

    arrayMaxConsecutiveSum completed

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,11 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+    let biggestSum: number = 0;
 
+    for(let i = 0; i < inputArray.length; i++){
+        if(inputArray[i+1] && inputArray[i] + inputArray[i + 1] > biggestSum){
+            biggestSum = inputArray[i] + inputArray[i + 1];
+        }
+    }
+    return biggestSum;
 }
-
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
