47f6f7c2f09fb4ab2358519369e3e3efd90ebf14
st-klockow <steve.klockow@gmail.com>
2021-05-16 01:20:56 +0200

    arrayMaximalAdjacentDifference finished over array iterated and max difference returned

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,12 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let maxDifference: number = 0;
 
+    for(let i = 0; i < inputArray.length; i++){
+        let difference = inputArray[i] - inputArray[i+1];
+        maxDifference = difference > maxDifference ? difference : maxDifference;
+    }
+
+    return maxDifference;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
