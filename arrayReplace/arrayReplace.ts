8b1084c7b0d0ff18f26aa949ac83229280f52138
st-klockow <steve.klockow@gmail.com>
2021-05-16 14:01:20 +0200

    arrayReplace finished loop through array and push numbers in to a new array, all occurrences of elemToReplace are replaced with substitutionElem

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,14 @@
 export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+    let newArray: number[] = [];
 
+    for(let i = 0; i < inputArray.length; i++){
+        if(inputArray[i] === elemToReplace){
+            inputArray[i] = substitutionElem;
+        }
+        newArray.push(inputArray[i]);
+    }
+
+    return newArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
