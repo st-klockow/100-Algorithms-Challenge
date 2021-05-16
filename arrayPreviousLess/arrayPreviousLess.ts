2c0e7afd00ab63b9da7e04b737ffe83acc681c2d
st-klockow <steve.klockow@gmail.com>
2021-05-16 12:55:07 +0200

    arrayPreviousLess
    loop through array and push numbers if there is a previous (smaller) number, if not, push -1 and return the new array

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,12 @@
 export function arrayPreviousLess(items: number[]): number[] {
+    let newItems: number[] = [];
+    for(let i = 0; i < items.length; i++){
+        if(items[i-1] < items[i]) newItems.push(items[i - 1])
+        else newItems.push(-1);
+    }
 
+    return newItems;
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+ console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+ console.log(arrayPreviousLess([32, 5, 22, 42, 5]));
